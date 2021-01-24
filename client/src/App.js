0932f2f8cfa9496fecb50d615e5d88b89ead1e88
vmangahis <vince.baron092299@gmail.com>
2021-01-25 03:18:37 +0800

    fixed components

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,21 @@ import SearchComponent from './components/SearchComponent';
 import AnimeList from './components/AnimeList';
 import Watchlist from'./components/Watchlist';
 
+
 function App() {
+
+  //Passing a function to setState actually has Previous state in the parameter
+
+  //useState(arrowFunction) - runs once 
+  //useState(function) - runs every render
+
+
   return (
     <div className='App'>
-      <h1>Anime Watchlist Web Application</h1>
+     
+      <h1>Anime Watchlist Application</h1>
       <SearchComponent />
+
       <AnimeList />
       <Watchlist />
     </div>
--- client/src/components/Searchbar.js
+++ client/src/components/Searchbar.js
@@ -3,9 +3,11 @@ import Button from './Button';
 const Searchbar = () => {
     return (
         <>
+            <div>
             <input type = "text" placeholder = "Search..." />
             <Button label = 'Search' />
             <Button label = 'Clear' />
+            </div>
         </>
     )
 }
