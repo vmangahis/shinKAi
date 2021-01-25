8eb817e10e9d434c9cbaf9f15cbf91d8f51268d1
vmangahis <vince.baron092299@gmail.com>
2021-01-25 17:20:45 +0800

    added mongoose and concurrently for full-stack run dev

---
+++ config/key.js
@@ -0,0 +1,4 @@
+module.exports = 
+{
+   URI: 'mongodb+srv://admin:admin123@animelist.hfmi5.mongodb.net/<dbname>?retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const exp = require('express');
+const mong = require('mongoose');
+const URI = require('./config/key').URI;
 const app = exp();
+
 const PORT = process.env.PORT || 5000;
 
+
+//connect to db
+
+
+mong.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => {
+    console.log('Connected to Anime Database!');
+})
+.catch(error =>{console.log(error)});
+;
+
+app.use(exp.json());
 
 app.get('/', (req, res) =>{
     console.log('request at root');
 });
+
+
 
 app.listen(PORT, (req, res) => {
     console.log(`Server listening to ${PORT}...`);
