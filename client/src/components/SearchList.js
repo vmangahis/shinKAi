175add665dcadd36fb6c457207c74ed2694637f8
vmangahis <vince.baron092299@gmail.com>
2021-02-18 02:15:59 +0800

    added static data for testing

--- client/src/components/SearchList.js
+++ client/src/components/SearchList.js
@@ -1,5 +1,7 @@
-import {Container, Table, Button} from 'reactstrap';
+import {Container, Table, Button, ListGroupItem, ListGroup} from 'reactstrap';
 import {useState} from 'react';
+import {CSSTransition, TransitionGroup} from 'react-transition-group';
+
 import{v4 as uuidv4} from 'uuid';
 
 const SearchList = () => {
@@ -8,10 +10,9 @@ const SearchList = () => {
     
     const initialState = [
 
-    
-            {id:   uuidv4(), anime_title: "AOT" , air_date: "2013-04-06T16:00:00.000+00:00 - 2013-04-06T16:00:00.000+00:00", episode: 25, status: "complete" },
-            {id:   uuidv4(), anime_title: "Chuuni" , air_date: "2013-04-06T16:00:00.000+00:00 - 2013-04-06T16:00:00.000+00:00", episode: 25, status: "complete"  },
-            {id:   uuidv4(), anime_title: "SAO"  , air_date: "2013-04-06T16:00:00.000+00:00 - 2013-04-06T16:00:00.000+00:00", episode: 25, status: "complete" }
+        {id:   uuidv4(), anime_title: "AOT"  , air_date: "2013-04-06T16:00:00.000+00:00 - 2013-04-06T16:00:00.000+00:00", episode: 25, status: "complete" , img_url: "https://i.imgur.com/7uOdUl5.jpg"},
+            {id:   uuidv4(), anime_title: "Chuuni" , air_date: "2013-04-06T16:00:00.000+00:00 - 2013-04-06T16:00:00.000+00:00", episode: 25, status: "complete", img_url: "https://i.imgur.com/7uOdUl5.jpg" },
+            {id:   uuidv4(), anime_title: "SAO"  , air_date: "2013-04-06T16:00:00.000+00:00 - 2013-04-06T16:00:00.000+00:00", episode: 25, status: "complete" , img_url: "https://i.imgur.com/7uOdUl5.jpg"}
         
     
     ];
@@ -22,7 +23,7 @@ const SearchList = () => {
         <Container>
             <Button color = "primary" onClick = {() => { const entry = prompt('Sample entry'); if(entry){
                 setAnimeList((list) => ({
-                    animeList:[...list, {id: uuidv4(), anime_title: entry, air_date: "2013-04-06T16:00:00.000+00:00 - 2013-04-06T16:00:00.000+00:00", episode: 25, status: "complete"}]
+                    animeList:[...list, {id: uuidv4(), anime_title: entry, air_date: "2013-04-06T16:00:00.000+00:00 - 2013-04-06T16:00:00.000+00:00", episode: 25, status: "complete", img_url: "https://i.imgur.com/7uOdUl5.jpg"}]
                 }));
             }}}>List Anime</Button>
             <Table dark>
@@ -34,8 +35,30 @@ const SearchList = () => {
                             <th>Episodes</th>
                             <th>Status</th>
                         </tr>
-
                     </thead>
+                    <tbody>
+                
+
+                {animeList.map(({id, anime_title, air_date, episode, status, img_url})=> (
+    
+                        <tr>
+                            <th scope="row"> <img 
+      src={img_url}
+      alt="new" width={200} height ={200}
+      /></th>
+                            <td>{anime_title}</td>
+                            <td>{air_date}</td>
+                            <td>{episode}</td>
+                            <td >{status}</td>
+                        </tr>
+                    
+        ))}
+
+
+            
+                </tbody>
+
+
 
 
 
