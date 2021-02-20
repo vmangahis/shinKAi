3b2141873354b88db6dd7f4a103e77bed47acaf2
vmangahis <vince.baron092299@gmail.com>
2021-02-20 20:19:44 +0800

    added modal form

--- client/src/components/ModalButton.js
+++ client/src/components/ModalButton.js
@@ -1,17 +1,35 @@
 import {useState} from 'react'
-import {Button, Modal} from 'reactstrap';
+import {Button, Modal, ModalHeader, ModalFooter, ModalBody, Form, FormGroup, Input, Label} from 'reactstrap';
 
 
 const ModalButton = () => {
 
     const [modalState, setModalState] = useState(false);
 
-    const toggle = () => setModalState(!modalState);
+    const toggleModal = () => setModalState(!modalState);
     
 
     return (
         <div>
-            <Button color = "primary">List Anime</Button>
+            <Button color = "primary" onClick = {() => setModalState(!modalState)}>Search Anime</Button>
+            <Modal isOpen ={modalState} toggle = {toggleModal}>
+            <ModalHeader toggle = {toggleModal}>Search Anime</ModalHeader>
+            <ModalBody>
+            <Form>
+                <FormGroup>
+                    <Label for="searchanime">Anime Title</Label>
+                    <Input type= "text" name="anime" id="searchanime" placeholder="Search..." />
+                </FormGroup>
+            </Form>
+            </ModalBody>
+
+            <ModalFooter>
+                <Button color="primary" onClick = {() => setModalState(false)}>Search</Button>
+                <Button color="secondary" onClick = {() => setModalState(false)}>Cancel</Button>
+            </ModalFooter>
+
+
+            </Modal>
         </div>
     )
 }
