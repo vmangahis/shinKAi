225a40f10dcffa62a22cacb4681ea161102814cb
vmangahis <vince.baron092299@gmail.com>
2021-03-05 02:45:50 +0800

    added add to watchlist function

--- client/src/components/SearchList.js
+++ client/src/components/SearchList.js
@@ -1,4 +1,4 @@
-import {Container, Table, Button} from 'reactstrap';
+import {Container, Table, Button, Modal, ModalBody, ModalFooter, ModalHeader} from 'reactstrap';
 import {useEffect, useState} from 'react';
 import {extAnime} from '../actions/AnimeAction';
 import {useDispatch, useSelector} from 'react-redux';
@@ -7,7 +7,12 @@ const SearchList = () => {
     
     const {anime} = useSelector(state => state.anime);
     const [searchState, setSearchState] = useState(anime);
+    const [modalPrompt, setModalPrompt] = useState(false);
     const dispatch = useDispatch();
+
+    const addToWatchList = () => {
+            setModalPrompt(!modalPrompt);
+    }
 
     useEffect(() => {
         setSearchState(state => ({...state,anime}))
@@ -16,6 +21,13 @@ const SearchList = () => {
     }, [anime])
     return (
         <>
+        <Modal isOpen = {modalPrompt} toggle = {addToWatchList}>
+            <ModalHeader toggle = {addToWatchList}>Add Anime to your Watchlist</ModalHeader>
+            <ModalFooter>
+                <Button color = "primary" onClick = {addToWatchList}>Add To Watchlist</Button>
+                <Button color = "warning" onClick = {addToWatchList}>Cancel</Button>
+            </ModalFooter>
+        </Modal>
         <Container>
             <h1>Search Table</h1>
             <Table dark responsive>
@@ -41,7 +53,7 @@ const SearchList = () => {
                         <td>{anime.airstart}</td>
                         <td>{anime.episodes}</td>
                         <td>{anime.status}</td>
-                        <td><Button color = "primary">Add To Watchlist</Button></td>
+                        <td><Button color = "primary" onClick={() => addToWatchList()}>Add To Watchlist</Button></td>
                                 </tr>
                             ))} 
                             
